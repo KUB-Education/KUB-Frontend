f3fd1ba3aa90237dfebcb2ad3864761fed1ef314
zhenya <zhenya.slisarenko@gmail.com>
2025-04-05 16:29:36 +0300

    navigation

--- src/app/ui/components/AuthorizedLayout/AuthorizedLayout.tsx
+++ src/app/ui/components/AuthorizedLayout/AuthorizedLayout.tsx
@@ -11,6 +11,7 @@ import {
   HeaderToolbar,
 } from './styles.tsx';
 import { Outlet } from 'react-router';
+import Breadcrumb from './Breadcrumb(navigation path)/Breadcrumb.tsx';
 
 const AuthorizedLayout = () => {
   const [open, setOpen] = useState(true);
@@ -34,7 +35,9 @@ const AuthorizedLayout = () => {
         }
       >
         <HeaderContent>
-          <HeaderLabel>Admin</HeaderLabel>
+          <HeaderLabel>
+            <Breadcrumb />
+          </HeaderLabel>
           <HeaderToolbar />
         </HeaderContent>
       </Header>
---
+++ src/app/ui/components/AuthorizedLayout/Breadcrumb(navigation path)/Breadcrumb.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const Breadcrumb = () => {
+  const location = useLocation();
+  const [breadcrumbKey, setBreadcrumbKey] = useState<string | null>(null);
+  const pathToKeyMap: Record<string, string> = {
+    '/home': 'Home',
+    '/students': 'Students',
+    '/student-groups': 'Student Groups',
+    '/lecturers': 'Lecturers',
+    '/subjects': 'Subjects',
+    '/rooms': 'Rooms',
+    '/educational-programs': 'Educational Programs',
+    '/departments': 'Departments',
+    '/educational-courses': 'Educational Courses',
+    '/timetables': 'Timetables',
+    '/help': 'Help',
+  };
+  useEffect(() => {
+    const currentPath = location.pathname;
+    if (pathToKeyMap[currentPath]) {
+      setBreadcrumbKey(pathToKeyMap[currentPath]);
+    } else {
+      setBreadcrumbKey(null);
+    }
+  }, [location.pathname, pathToKeyMap]);
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      Admin
+      <div
+        style={{ fontSize: '16px', margin: '0 10px', verticalAlign: 'middle' }}
+      >
+        {' > '}
+      </div>
+      {breadcrumbKey}
+    </div>
+  );
+};
+
+export default Breadcrumb;
